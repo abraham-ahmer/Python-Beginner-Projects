f01ba5b259fc3f360d97767d91d1b68c30e3df87
Abraham Ahmer <utdahmer@gmail.com>
2026-03-27 11:51:23 +0500

    Changed the whole code and added a better, real life version of shopping cart with OOP.

--- shopping-cart/cart.py
+++ shopping-cart/cart.py
@@ -1,18 +1,51 @@
-shop = {
-    "apple" : 2,
-    "banana" : 1.5,
-    "carrot" : 1
-}
-cart_val = 0 #initializing val as 0, so that we can add value afterwards.
+#23. Implement a class ShoppingCart with methods to add items, remove items, calculate total, and apply discounts.
 
-while True: #infinite True, so that user can put as many items he can.
+class ShoppingCart:
+    def __init__(self):
+        self.items = []
+    
+    # add items
+    def add_items(self, *items):
+        for item, price, qty in items:
+            self.items.append({"item": item, "price": price, "quantity": qty})
+    
+    # remove items
+    def remove_items(self, remove):
+        self.remove = remove
+        for i in self.items:
+            if self.remove in i["item"]:
+                self.items.remove(i)
+                return "item removed from the list"
+        else:
+            return "item your are trying to remove does not exist in cart"
+    
+    # calculate total
+    def calculate_total(self):
+        price = 0
+        for i in self.items:
+            price +=  i["price"] * i["quantity"]
+        return price
+    
+    # show total
+    def show_total(self):
+        return f"Total: {self.calculate_total()} USD"
+    
+    # add discount
+    def discount(self):
+        if self.calculate_total() >= 100: 
+            return f"After 10% Discount: {self.calculate_total() - (self.calculate_total() * 0.1)}" # 10% if total = 100 or + 
+        else:
+            return f"After 5% Discount: {self.calculate_total() - (self.calculate_total() * 0.05)}" # 5% if less than 100
 
-    items = input("Add items on your cart: ").lower()
-    if items == "done": #stopping condition
-        break
-    elif items in shop: 
-            cart_val += shop[items] #if items found in shop, then add their val with each others using cart_value
-    else:
-        print("Not Availble. Kindly Choose From The Available Items.") #if items arent in shop, ask them again.
+s1 = ShoppingCart()
 
-print(f"Final Cart Value = {cart_val}") #final spending
+
+s1.add_items(("chocolate", 10,2), ("pepsi",25,2), ("chicken",50,1)) # add 
+print(s1.items)
+
+print(s1.remove_items("chocolate")) # remove
+print(f"Cart: {s1.items}")
+
+print(s1.show_total())
+
+print(s1.discount())
